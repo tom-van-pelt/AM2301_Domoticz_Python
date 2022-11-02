aa39a07aae931b8287a0860fa129c291f7a17c16
Tom <tomtcvanpelt@gmail.com>
2022-11-02 19:42:29 +0100

    cleaned up code

--- am2301.py
+++ am2301.py
@@ -5,14 +5,15 @@ import sys
 import urllib.request
 import numbers
 
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
-dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False) #board.D<number> where <number> is GPIO pin
-domoticz_sensor_idx = 7 #change to dummy sensor idx (Temp+Hum)
-printData = True #change to false if you don't want to print information to console
+#change data here / verander data hieronder:
+dhtDevice = adafruit_dht.DHT22 (board.D4, use_pulseio=False) #board.D<number> where <number> is GPIO pin | use_pulsio = false for Raspberry Pi
+domoticz_sensor_idx = 7 #change to dummy sensor idx (Temp+Hum idx)
+printData = True #change to False if you don't want to print information to console
+update_interval = 60.0 #seconds | standard 60 seconds
 
-#make sure 127.0.0.1 is local IP-address in Setup -> Settings -> Local Networks
+# ================================================================================================================
+
+#make sure 127.0.0.1 is local IP-address in Domoticz (Setup -> Settings -> Local Networks)
 url_json = "http://127.0.0.1:8080/json.htm?type=command&param=udevice&idx="
 
 while True:
@@ -21,7 +22,7 @@ while True:
         temperature_c = dhtDevice.temperature
         humidity = dhtDevice.humidity
 
-        if isinstance(temperature_c, numbers.Number) and isinstance(humidity, numbers.Number):
+        if isinstance (temperature_c, numbers.Number) and isinstance (humidity, numbers.Number):
             cmd = url_json + str (domoticz_sensor_idx) + "&nvalue=0&svalue=" + str (temperature_c) + ";" + str (humidity) + ";0"
             print ("Url:", cmd)
             send_data = urllib.request.urlopen (cmd)
@@ -33,12 +34,13 @@ while True:
 
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-        time.sleep(1.0)
+        print (error.args[0])
+        time.sleep (1.0)
         continue
+
     except Exception as error:
-        dhtDevice.exit()
+        dhtDevice.exit ()
         raise error
 
     #update interval (seconds)
-    time.sleep(30.0)
+    time.sleep (update_interval)
